506619db99b0b63f9964c9d1fda769482b497145
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2023-05-23 13:02:24 +0300

    add gh-pages link to cors

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,6 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
+  console.log(message);
   res.status(statusCode).send({
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
